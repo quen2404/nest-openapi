ab4a416488c17473b58d297030d47307dce9f2d9
Quentin Desrame <quen2404@gmail.com>
2018-11-09 09:15:16 +0100

    fix(model): add missing property components in openapi

--- lib/model/openapi.model.ts
+++ lib/model/openapi.model.ts
@@ -4,11 +4,13 @@ import { Paths } from './paths.model';
 import { SecurityRequirement } from './security-requirement.model';
 import { Tag } from './tag.model';
 import { ExternalDocumentation } from './external-doc.model';
+import { Components } from './components.model';
 export interface OpenAPI {
   openapi: string;
   info: Info;
   servers: Server[];
   paths: Paths;
+  components: Components;
   security: SecurityRequirement[];
   tags: Tag[];
   externalDocs: ExternalDocumentation;
