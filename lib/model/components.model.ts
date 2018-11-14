83d00d0209a30eb605d9d5c972a3fb0aecd76926
Quentin Desrame <quen2404@gmail.com>
2018-11-14 01:09:05 +0100

    feat(model): add class-transformer decorators to components

--- lib/model/components.model.ts
+++ lib/model/components.model.ts
@@ -7,14 +7,24 @@ import { Header } from './header.model';
 import { SecurityScheme } from './security-scheme.model';
 import { Link } from './link.model';
 import { Callback } from './callback.model';
+import { Type } from 'class-transformer';
 export class Components {
+  @Type(() => Schema)
   schemas: Map<string, Schema>;
+  @Type(() => Response)
   responses: Map<string, Response>;
+  @Type(() => Parameter)
   parameters: Map<string, Parameter>;
+  @Type(() => Example)
   examples: Map<string, Example>;
+  @Type(() => RequestBody)
   requestBodies: Map<string, RequestBody>;
+  @Type(() => Header)
   headers: Map<string, Header>;
+  @Type(() => SecurityScheme)
   securitySchemes: Map<string, SecurityScheme>;
+  @Type(() => Link)
   links: Map<string, Link>;
+  @Type(() => Callback)
   callbacks: Map<string, Callback>;
 }
