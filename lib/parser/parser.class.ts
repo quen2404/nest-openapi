c24acf81481cb95d8df5651dce21e67709135daa
Quentin Desrame <quen2404@gmail.com>
2018-11-21 01:53:03 +0100

    refactor(parser): simplify readFromFile method

--- lib/parser/parser.class.ts
+++ lib/parser/parser.class.ts
@@ -5,7 +5,6 @@ import { plainToClass } from 'class-transformer';
 
 export class Parser {
   static readFromFile(filepath: string): OpenAPI {
-    var doc = safeLoad(readFileSync(filepath, 'utf8'));
-    return plainToClass(OpenAPI, doc as OpenAPI);
+    return plainToClass(OpenAPI, safeLoad(readFileSync(filepath, 'utf8')) as OpenAPI);
   }
 }
