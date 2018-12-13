4eb173da0100a102966d98912a5ff42002f7c2b4
Quentin Desrame <quen2404@gmail.com>
2018-12-13 23:02:51 +0100

    feat(generator): prepare multiple response choice

--- lib/generator/response.generator.ts
+++ lib/generator/response.generator.ts
@@ -6,6 +6,10 @@ export class ResponseGenerator {
   public constructor(private outputPath: string, private openapi: OpenAPI, private mediaTypeGen: MediaTypeGenerator) {}
   public getTypeFromResponses(responses: Map<string, Response>): SchemaType {
     // TODO
+    const count = responses.size - (responses.has('default') ? 1 : 0);
+    if (count > 1) {
+      // There is more than one type of response, we have to use Response<?>
+    }
     let type: SchemaType = null;
     if (responses != null) {
       responses.forEach((response, code) => {
@@ -28,6 +32,10 @@ export class ResponseGenerator {
     let type: SchemaType = null;
     if (response.content != null) {
       type = this.mediaTypeGen.getTypeFromMediaType(response.content.get('application/json'));
+    }
+    if (response.headers != null && response.headers.size > 0) {
+      // We have to is more than one type of response, we have to use Response<?>
+      // type =
     }
     if (type == null) {
       return TYPE_ANY;
