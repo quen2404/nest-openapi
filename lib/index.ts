6ee5b96de162d70d488af77acb3193b92506d6cb
Quentin Desrame <quen2404@gmail.com>
2018-12-18 23:53:16 +0100

    fix: add missing exports

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,7 @@ export * from './generator';
 export * from './model';
 export * from './parser';
 export * from './utils';
+export * from './openapi.constants';
+export * from './openapi.explorer';
+export * from './openapi.factory';
+export * from './openapi.module';
